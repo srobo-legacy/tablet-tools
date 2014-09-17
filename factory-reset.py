aff716d15ae5e5117f15e05b2b8924986b260ce1
Tom Leese <tom@tomleese.me.uk>
2014-09-17 16:26:07 +0100

    Update factory reset script

--- factory-reset.py
+++ factory-reset.py
@@ -1,22 +1,34 @@
 #!/usr/bin/env python3
 import subprocess
+import sys
+import time
 
 
 def wait_for_device():
     """Block until a device has appeared in the Android Debug Bridge."""
-    print("Waiting for a device…")
+    print("Waiting for a device… ", end="")
+    sys.stdout.flush()
     exit_code = subprocess.call(["adb", "wait-for-device"])
+    assert exit_code == 0
+    print("found!")
+
+
+def reboot(target):
+    print("Rebooting to {target}.".format(target=target))
+    exit_code = subprocess.call(["adb", "reboot", target])
     assert exit_code == 0
 
 
-def factory_reset():
-    print("Performing factory reset.")
-    process = subprocess.Popen(["adb", "shell"], stdout=subprocess.PIPE,
-                               stdin=subprocess.PIPE, stderr=subprocess.PIPE)
-    process.communicate(input="recovery --wipe_data")
+def recovery_wipe_data():
+    print("Wiping data.")
+    exit_code = subprocess.call(["adb", "shell", "recovery", "--wipe_data"])
+    assert exit_code == 0
 
 
 while True:
     wait_for_device()
-    factory_reset()
+    reboot("recovery")
+    # wait_for_device()
+    time.sleep(30)
+    recovery_wipe_data()
     input("Press return to continue.")
